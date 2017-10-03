4191e6ff05e1dd3d98bc5f10bc1352f8dfa9efd3
Gus <yo@soygus.com>
2017-10-03 08:23:53 -0600

    Passes facebook secret to env

--- app/Service/Facebook/FacebookService.php
+++ app/Service/Facebook/FacebookService.php
@@ -9,8 +9,8 @@ class FacebookService
     public function __construct()
     {
         $this->fb = new \Facebook\Facebook([
-          'app_id' => '120678928645856',
-          'app_secret' => '98156b460bd61df3dc1fb6b2f94a147f',
+          'app_id' => env('FACEBOOK_APP_ID'),
+          'app_secret' => env('FACEBOOK_APP_SECRET'),
           'default_graph_version' => 'v2.10',
         ]);
     }
