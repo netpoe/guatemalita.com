aa92b288050b699ad9f20bcc00288c3decda6c06
Gus <yo@soygus.com>
2017-10-23 11:48:55 -0600

    Fixes Mailchimp NL action

--- app/Http/Controllers/Front/MailchimpController.php
+++ app/Http/Controllers/Front/MailchimpController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Front\MailchimpSubscriptionRequest;
+use Mailchimp\Mailchimp;
 
 class MailchimpController extends Controller
 {
@@ -23,8 +24,57 @@ class MailchimpController extends Controller
 
         $result = $mc->get("lists/{$listId}/members", []);
 
-        print_r($result); exit;
-
         return view('front.mailchimp.confirmation');
     }
+
+    public function sendEventsNewsletter(Mailchimp $mc)
+    {
+        try {
+            $today = date('Y-m-d');
+            $mailchimpListId = env('MAILCHIMP_LIST_ID');
+            $mailchimpTemplateId = (int) env('MAILCHIMP_EVENTS_TEMPLATE_ID');
+
+            $result = $mc->post('campaigns', [
+                'type' => 'regular',
+                'recipients' => [
+                    'list_id' => $mailchimpListId,
+                ],
+                'settings' => [
+                    'subject_line' => 'Calendario Guatemalita',
+                    'title' => "Guatemalita $today",
+                    'from_name' => 'Guatemalita.com',
+                    'reply_to' => 'hola@guatemalita.com',
+                    'template_id' => $mailchimpTemplateId,
+                ],
+            ]);
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+            print_r($e->getMessage()); exit;
+            // TODO send me an email if there's an error
+        }
+
+        try {
+            $campaignId = $result['id'];
+
+            $result = $mc->post("/campaigns/{$campaignId}/actions/send");
+        } catch (\Exception $e) {
+            error_log($e->getMessage());
+            print_r($e->getMessage()); exit;
+            // TODO send me an email if there's an error
+        }
+
+        print_r($result); exit;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,25 @@ Route::get('/', function () {
 });
 
 Route::group(['namespace' => 'Front'], function(){
-    Route::post('/mailchimp/subscribe', 'MailchimpController@subscribe')->name('front.mailchimp.subscribe');
-    Route::get('/newsletter/confirmacion', 'MailchimpController@confirmation')->name('front.mailchimp.confirmation');
     Route::get('/', 'HomeController@index')->name('front.home.index');
     Route::get('/notas/{post?}', 'PostsController@index')->name('front.posts.index');
+});
+
+/**
+ * FRONT:Newsletter
+ */
+Route::group(['namespace' => 'Front'], function(){
+    Route::post('/mailchimp/subscribe', 'MailchimpController@subscribe')->name('front.mailchimp.subscribe');
+    Route::get('/newsletter/confirmacion', 'MailchimpController@confirmation')->name('front.mailchimp.confirmation');
+    Route::get('/newsletter/send-events-newsletter', 'MailchimpController@sendEventsNewsletter')->name('front.events.send-events-newsletter');
+});
+
+/**
+ * FRONT:EVENTS
+ */
+Route::group(['namespace' => 'Front'], function(){
     Route::get('/eventos', 'EventsController@index')->name('front.events.index');
     Route::get('/eventos/rss-feed', 'EventsController@rssFeed')->name('front.events.rss-feed');
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
